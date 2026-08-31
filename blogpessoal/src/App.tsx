b8017995224dd5d91144a042f51d53e8749b8b3c
Higor <higordamascenostella@gmail.com>
2026-08-30 22:44:02 -0300

    :sparkles: Deletar feito

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,35 +1,41 @@
-import Home from "./pages/home/Home";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Cadastro from "./pages/cadastro/Cadastro";
-import Login from "./pages/login/Login";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
+import Home from "./pages/home/Home"
+import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login"
+import { AuthProvider } from "./contexts/AuthContext"
+import DeletarTema from "./components/tema/deletartema/DeletarTema"
+import FormTema from "./components/tema/formtema/FormTema"
+import ListaTemas from "./components/tema/listatemas/ListaTemas"
+import ListaPostagens from "./components/postagem/listapostagem/ListaPostagem"
+import FormPostagem from "./components/postagem/formpostagem/FormPostagem"
+import DeletarPostagem from './components/postagem/deletarpostagem/DeletarPostagem'
 
 function App() {
-  return (
-    <>
-
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-
-        
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/cadastro' element={<Cadastro />} />
-        </Routes>
-        </div>
-        <Footer />
-
-      </BrowserRouter>
-
-
-    </>
-
-
-  )
+	return (
+		<AuthProvider>
+			<BrowserRouter>
+				<Navbar />
+				<div className="min-h-[80vh]">
+					<Routes>
+						<Route path="/" element={<Login />} />
+						<Route path="/home" element={<Home />} />
+						<Route path="/cadastro" element={<Cadastro />} />
+						<Route path="/temas" element={<ListaTemas />} />
+						<Route path="/cadastrartema" element={<FormTema />} />
+						<Route path="/editartema/:id" element={<FormTema />} />
+						<Route path="/deletartema/:id" element={<DeletarTema />} />
+						<Route path="/postagens" element={<ListaPostagens />} />
+						<Route path="/cadastrarpostagem" element={<FormPostagem />} />
+						<Route path="/editarpostagem/:id" element={<FormPostagem />} />
+						<Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+					</Routes>
+				</div>
+				<Footer />
+			</BrowserRouter>
+		</AuthProvider>
+	)
 }
 
 export default App
--- blogpessoal/src/pages/home/Home.tsx
+++ blogpessoal/src/pages/home/Home.tsx
@@ -1,41 +1,47 @@
+import ListaPostagens from "../../components/postagem/listapostagem/ListaPostagem"
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem"
+
 function Home() {
   return (
     // Conteiner Principal
-    <div className="bg-indigo-900 flex justify-center">
+    <>
+      <div className="bg-indigo-900 flex justify-center">
 
-      {/* Seção com duas colunas */}
-      <div className="container grid grid-cols-1 md:grid-cols-2 text-white">
+        {/* Seção com duas colunas */}
+        <div className="container grid grid-cols-1 md:grid-cols-2 text-white">
 
-        {/* Conteúdo de texto */}
-        <div className="flex flex-col gap-4 items-center justify-center py-4">
-          <h2 className="text-2xl md:text-5xl font-bold">
-            Seja Bem Vindo(a)!
-          </h2>
+          {/* Conteúdo de texto */}
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className="text-2xl md:text-5xl font-bold">
+              Seja Bem Vindo(a)!
+            </h2>
 
-          <p className="text-xl">
-            Expresse aqui seus pensamentos
-          </p>
+            <p className="text-xl">
+              Expresse aqui seus pensamentos
+            </p>
 
-          <div className="flex justify-around gap-4">
-            <div className="rounded-lg border-2 border-solid border-white py-2 px-4">
-              Nova Postagem
+            <div className="flex justify-around gap-4">
+              <ModalPostagem />
+            
             </div>
           </div>
+
+          {/* Imagem da Página home */}
+          <div className="flex justify-center">
+            <img
+              src="https://i.imgur.com/fyfri1v.png"
+              alt="Imagem da página Home"
+              className="w-2/3"
+            />
+          </div>
+
         </div>
 
-        {/* Imagem da Página home */}
-        <div className="flex justify-center">
-          <img
-            src="https://i.imgur.com/fyfri1v.png"
-            alt="Imagem da página Home"
-            className="w-2/3"
-          />
-        </div>
 
       </div>
 
-
-    </div>
+      <ListaPostagens />
+    </>
   )
 }
 
